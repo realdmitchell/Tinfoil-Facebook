1d1c1c2bd07b837d100fe0b8ea33f19e1a17b0ef
Real D mitchell <rdmitch@example.com>
2015-11-14 22:12:18 +0100

    rename the project to logins

---
+++ app/src/main/java/com/realdm/fbwrapper/webview/FacebookWebViewClient.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) 2013 Daniel Velazco
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.realdm.fbwrapper.webview;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.view.KeyEvent;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.realdm.fbwrapper.util.Logger;
+
+import java.io.ByteArrayInputStream;
+
+/**
+ * FacebookWebViewClient.<br/>
+ * Extends {@link android.webkit.WebViewClient}.<br/>
+ * Used by {@link FacebookWebView}.
+ */
+public class FacebookWebViewClient extends WebViewClient {
+
+    // Members
+    private WebViewClientListener mListener = null;
+    private boolean mAllowAnyUrl = false;
+
+    /**
+     * Set the listener for this WebViewClient.
+     *
+     * @param listener {@link WebViewClientListener}. It must be
+     *                 in the Activity context.
+     */
+    public void setListener(WebViewClientListener listener) {
+        mListener = listener;
+    }
+
+    /**
+     * Destroy WebViewClient instance.
+     */
+    public void destroy() {
+        mListener = null;
+    }
+
+    /**
+     * Whether this WebViewClient should load any domain without
+     * checking the internal domain list.
+     *
+     * @param allow {@link boolean}
+     */
+    public void setAllowAnyDomain(boolean allow) {
+        mAllowAnyUrl = allow;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onReceivedError(WebView view, int errorCod, String description, String failingUrl) {
+        Logger.e(getClass().getSimpleName(), "This WebView has received an error while trying to load:");
+        Logger.e(getClass().getSimpleName(), "\tError code: " + errorCod);
+        Logger.e(getClass().getSimpleName(), "\tDescription: " + description);
+        Logger.e(getClass().getSimpleName(), "\tFailed URL: " + failingUrl);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public WebResourceResponse shouldInterceptRequest (final WebView view, String url) {
+        // We are currently not intercepting any resources
+
+//        if ((url.startsWith("data:"))
+        if ((url.contains("ars"))
+                || (url.contains("arstechnica"))
+                || (url.contains("ebay"))
+                || (url.contains("golem"))
+                || (url.contains("bbc"))
+                || (url.contains("xda"))
+                ){
+            return super.shouldInterceptRequest(view, url);
+        }else
+        {
+            Uri uri = Uri.parse(url);
+            Logger.d(getClass().getSimpleName(), "External URL Blocking " + url);
+            return new WebResourceResponse("text/plain", "utf-8",
+                    new ByteArrayInputStream(("[URL blocked \n" + uri.getHost() + "]").getBytes()));
+        }
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+        // Allow the WebView to handle all KeyEvents for now
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        Logger.d(getClass().getSimpleName(), "shouldOverrideUrlLoading? " + url);
+
+
+        // Avoid NPEs when clicking on weird blank links
+        if (url.equals("about:blank")) {
+            Logger.d(getClass().getSimpleName(), "Blank page, let it load");
+            return false;
+        }
+
+
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+
+        // Fire the callback
+        fireOnPageStartedListener(url);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+
+        // Fire the callback
+        fireOnPageFinishedListener(url);
+    }
+
+    /**
+     * Fire off the onPageLoadStarted callback.
+     *
+     * @param url {@link String} The url that just started loading.
+     */
+    private void fireOnPageStartedListener(String url) {
+        if (mListener != null) {
+            mListener.onPageLoadStarted(url);
+        }
+    }
+
+    /**
+     * Fire off the onPageLoadFinished callback.
+     *
+     * @param url {@link String} The url that just finished loading.
+     */
+    private void fireOnPageFinishedListener(String url) {
+        if (mListener != null) {
+            mListener.onPageLoadFinished(url);
+        }
+    }
+
+    /**
+     * Fire off the openExternalSite callback.
+     * The listener {@link Activity} should just hand off this
+     * url as an intent to make sure some other app can handle it.
+     */
+    private void fireOpenExternalSiteListener(String url) {
+        if (mListener != null) {
+            mListener.openExternalSite(url);
+        }
+    }
+
+    /**
+     * Listener interface used to fire callbacks for page load
+     * started, finished as well as handing off opening an external url
+     */
+    public interface WebViewClientListener {
+
+        /**
+         * Notify the host activity that a page has started loading.
+         *
+         * @param url {@link String} The url that just started loading.
+         */
+        void onPageLoadStarted(String url);
+
+        /**
+         * Notify the host activity that a page has finished loading.
+         *
+         * @param url {@link String} The url that just finished loading.
+         */
+        void onPageLoadFinished(String url);
+
+        /**
+         * Notify the host activity that it should hand off this URL
+         * as an intent to any application that can open it since it
+         * won't be handled by us.
+         *
+         * @param url {@link String} The url to be loaded.
+         */
+        void openExternalSite(String url);
+
+    }
+}
